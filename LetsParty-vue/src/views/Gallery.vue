85b33e2b33b9df3556af8fdd10d41c726aa2c0e0
Dylan Faulk <dfaulk3446@gmail.com>
2026-07-30 09:55:52 -0700

    cut and reduced photo count

--- LetsParty-vue/src/views/Gallery.vue
+++ LetsParty-vue/src/views/Gallery.vue
@@ -1,49 +1,39 @@
 <script setup lang="ts">
-const galleryLandscape = [
+
+
+
+const gallerySmall = [
+  '/Let%E2%80%99s%20party/IMG_8745.jpg',
+  '/Let%E2%80%99s%20party/IMG_0575.JPG',
+  '/Let%E2%80%99s%20party/IMG_3173.jpg',
+  '/Let%E2%80%99s%20party/IMG_2629.jpg',
+  '/Let%E2%80%99s%20party/IMG_2643.jpg',
+  '/Let%E2%80%99s%20party/IMG_4402.jpg',
+  '/Let%E2%80%99s%20party/IMG_0707.JPG',
+  '/Let%E2%80%99s%20party/IMG_6025.JPG',
+  '/IMG_6043.jpg',
+  '/Let%E2%80%99s%20party/IMG_6029.jpg',
+  '/Let%E2%80%99s%20party/IMG_6032.jpg',
+  '/Let%E2%80%99s%20party/IMG_3690.jpg',
+
+]
+const galleryMedium = [
+  '/Let%E2%80%99s%20party/IMG_3160.jpg',
+  '/Let%E2%80%99s%20party/IMG_7621.jpg',
   '/MJ-Bday.jpeg',
   '/HeroPicture.jpg',
   '/shoot1.JPG',
   '/Let%E2%80%99s%20party/DSC_0218.jpeg',
-  '/Let%E2%80%99s%20party/IMG_0707.JPG',
-  '/Let%E2%80%99s%20party/IMG_6025.JPG',
-]
-
-const galleryPortrait = [
-  '/IMG_6043.jpg',
-  '/IMG_6972.JPG',
-  '/IMG_6981.JPG',
-  '/Let%E2%80%99s%20party/IMG_6029.jpg',
-  '/Let%E2%80%99s%20party/IMG_6030.jpg',
-  '/Let%E2%80%99s%20party/IMG_6032.jpg',
-  '/Let%E2%80%99s%20party/IMG_6039.jpg',
-  '/Let%E2%80%99s%20party/IMG_6024.jpg',
-  '/Let%E2%80%99s%20party/IMG_3690.jpg',
-  '/Let%E2%80%99s%20party/IMG_6042.jpg',
-  '/Let%E2%80%99s%20party/IMG_6043.jpg',
-  '/Let%E2%80%99s%20party/IMG_6048.jpg',
-  '/Let%E2%80%99s%20party/IMG_6969.JPG',
-  '/Let%E2%80%99s%20party/IMG_6972.JPG',
-  '/Let%E2%80%99s%20party/IMG_6981.JPG',
-  '/Let%E2%80%99s%20party/IMG_6982.JPG',
-  '/Let%E2%80%99s%20party/IMG_7115.JPG',
-  '/Let%E2%80%99s%20party/IMG_7621.jpg',
-  '/Let%E2%80%99s%20party/IMG_7622.jpg',
-  '/Let%E2%80%99s%20party/IMG_8745.jpg',
-  '/Let%E2%80%99s%20party/IMG_8760.jpg',
-  '/Let%E2%80%99s%20party/IMG_0562.JPG',
-  '/Let%E2%80%99s%20party/IMG_0575.JPG',
-  '/Let%E2%80%99s%20party/IMG_0582.JPG',
-  '/Let%E2%80%99s%20party/IMG_2629.jpg',
-  '/Let%E2%80%99s%20party/IMG_2643.jpg',
-  '/Let%E2%80%99s%20party/IMG_3160.jpg',
-  '/Let%E2%80%99s%20party/IMG_3173.jpg',
-  '/Let%E2%80%99s%20party/IMG_3345.jpg',
-  '/Let%E2%80%99s%20party/IMG_3424.jpg',
   '/Let%E2%80%99s%20party/IMG_3432.jpg',
   '/Let%E2%80%99s%20party/IMG_3692.jpg',
   '/Let%E2%80%99s%20party/IMG_3983.jpg',
-  '/Let%E2%80%99s%20party/IMG_4402.jpg',
+  '/Let%E2%80%99s%20party/IMG_3345.jpg',
 ]
+const galleryLarge = [
+
+
+]
+
 </script>
 
 <template>
@@ -59,12 +49,12 @@ const galleryPortrait = [
       <h2>Event styling in action</h2>
     </div>
     <div class="gallery-grid">
-      <article v-for="img in galleryLandscape" :key="img" class="gallery-card">
+      <article v-for="img in galleryMedium" :key="img" class="gallery-card">
         <img :src="img" :alt="'Gallery image ' + img" class="gallery-image" />
       </article>
     </div>
       <div class="gallery-grid-port">
-      <article v-for="img in galleryPortrait" :key="img" class="gallery-card-port">
+      <article v-for="img in gallerySmall" :key="img" class="gallery-card-port">
         <img :src="img" :alt="'Gallery image ' + img" class="gallery-image-port" />
       </article>
     </div>
